5ae2283ba8f0769b0af59f0e943eb907dcfd707d
kathiaNeira <kathianeira@outlook.com>
2020-07-09 18:07:20 -0500

    feat(): basic webpack configuration

---
+++ app/webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html')
+    })
+  ]
+}
